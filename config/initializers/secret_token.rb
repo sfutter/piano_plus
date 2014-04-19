1c5199e8701aa492c30d79e23b2338ba584ebfd6
Steven Futter <futter.steven@gmail.com>
2014-04-19 14:17:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PianoPlus::Application.config.secret_key_base = '143c7d2b5ac89f59fb6443ad5cf4a1eee89e3f9aa637961e4ed7b349520e92e6becd2199f9f7cfab7dd4045c731e7851967fc217d261a7d0777ddbac9cd4d1fe'
